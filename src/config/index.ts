cedb1dbdbf62cdd18cc29bc7cd08c56d314998a5
Luis Cereño <luis.cereno.t@gmail.com>
2024-09-05 18:19:30 -0400

    Se cambia nombre de archivo con uuid y se validan extensiones, para que solo se acepten extensiones validas

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,4 +2,5 @@ export * from './envs';
 export * from './validators';
 export * from './jwt.adapter';
 export * from './regular-exp';
+export * from './uuid.adapter';
 export * from './bcrypt.adapter';
--- src/presentation/services/file-upload.service.ts
+++ src/presentation/services/file-upload.service.ts
@@ -1,10 +1,14 @@
 import path from 'path';
 import fs from 'fs';
 import { UploadedFile } from 'express-fileupload';
+import { Uuid } from '../../config';
+import { CustomError } from '../../domain';
 
 export class FileUploadService {
 
-    constructor() {};
+    constructor(
+        private readonly uuid = Uuid.v4,
+    ) {};
 
     private checkFolder( folderPath: string ) {
         if ( !fs.existsSync( folderPath ) ) {
@@ -18,13 +22,21 @@ export class FileUploadService {
         validExtensions: string[] = ['png', 'jpg', 'jpeg', 'gif'] 
     ) {
         try {
-            const fileExtension = file.mimetype.split( '/' ).at( 1 );
+            const fileExtension = file.mimetype.split( '/' ).at( 1 ) || '';
+            if ( !validExtensions.includes( fileExtension ) ) {
+                throw CustomError.badRequest( `Invalid extension: ${ fileExtension }, invalid ones ${ validExtensions }` )       
+            };
+
             const destination = path.resolve( __dirname, '../../../', folder );
             this.checkFolder( destination );
 
-            file.mv( destination + '/mi-imagen')
+            const fileName = `${ this.uuid() }.${ fileExtension }`;
+            file.mv( `${ destination }/${ fileName }` );
+
+            return { fileName };
         } catch ( error ) {
             console.log({ error });
+            throw error;
         };
     };
 
