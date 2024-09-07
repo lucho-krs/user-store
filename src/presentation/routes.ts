0b6fc3a76e31d9419abcfee5d6d76a9de384c9b3
Luis Cereño <luis.cereno.t@gmail.com>
2024-09-07 16:35:25 -0400

    Se crea retorno de imagenes por producto, usuario y categoria

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -3,6 +3,7 @@ import { AuthRoutes } from './auth/routes';
 import { CategoryRoutes } from './category/routes';
 import { ProductRoutes } from './product/routes';
 import { FileUploadRoutes } from './file-upload/routes';
+import { ImageRoutes } from './images/routes';
 
 export class AppRoutes {
 
@@ -13,6 +14,7 @@ export class AppRoutes {
         router.use( '/api/products', ProductRoutes.routes );
         router.use( '/api/categories', CategoryRoutes.routes );
         router.use( '/api/upload', FileUploadRoutes.routes );
+        router.use( '/api/images', ImageRoutes.routes );
 
         return router;
 
